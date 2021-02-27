1bb16c65ce0f6ae97dbff52aaa48a371fec9ab18
Avani Hall <avanichall@gmail.com>
2021-02-26 20:38:54 -0500

    Unit 2 Assignment

---
+++ flixpart1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixpart1
+//
+//  Created by Avani Hall on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
